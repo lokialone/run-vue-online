0c438af663010334b8a6977a60fed5f69ccdd0e4
loki <648875862@qq.com>
2019-08-01 15:16:40 +0800

    feat: 更新生成d.ts文件的位置

--- packages/sfc-parser/types/html-parser.d.ts
+++
@@ -1,8 +0,0 @@
-/*!
- * HTML Parser By John Resig (ejohn.org)
- * Modified by Juriy "kangax" Zaytsev, Evan You and Vue.js community
- * Original code by Erik Arvidsson, Mozilla Public License
- * http://erik.eae.net/simplehtmlparser/simplehtmlparser.js
- */
-export declare const isPlainTextElement: (key: string) => true | void;
-export declare function parseHTML(html: any, options: any): void;
